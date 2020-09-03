f426ced1ba3ed7b7a24f6e182550557d765a80b7
Samuel Cabrero <scabrero@suse.de>
2020-09-03 15:12:26 +0200

    samba: Remove xfs from samba_local
    Recent dracut versions throw an error when specifying a builtin module
    in the --add-drivers argument. Remove xfs from samba_local as the bundled
    kernel config files build xfs builtin.
    
    Link https://github.com/rapido-linux/rapido/issues/130
    
    Signed-off-by: Samuel Cabrero <scabrero@suse.de>

--- cut/samba_local.sh
+++ cut/samba_local.sh
@@ -32,7 +32,7 @@ _rt_require_conf_dir SAMBA_SRC
 		  "/lib/dracut/hooks/emergency/00-rapido-init.sh" \
 	--include "$RAPIDO_DIR/rapido.conf" "/rapido.conf" \
 	--include "$RAPIDO_DIR/vm_autorun.env" "/vm_autorun.env" \
-	--add-drivers "zram lzo lzo-rle xfs btrfs" \
+	--add-drivers "zram lzo lzo-rle btrfs" \
 	--modules "bash base" \
 	$DRACUT_EXTRA_ARGS \
 	$DRACUT_OUT || _fail "dracut failed"
