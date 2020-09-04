479310598ece6bd6d5b3d1ad4933ab793ed44b41
Samuel Cabrero <scabrero@suse.de>
2020-09-04 11:03:05 +0200

    Add cifs_witness environment
    The VM will mount a cifs share and start the swnd daemon to receive the
    witness notifications from the cluster.
    
    Signed-off-by: Samuel Cabrero <scabrero@suse.de>

---
+++ autorun/cifs_witness.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+#
+# Copyright (C) SUSE LINUX GmbH 2020, all rights reserved.
+#
+# This library is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation; either version 2.1 of the License, or
+# (at your option) version 3.
+#
+# This library is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+# License for more details.
+
+if [ ! -f /vm_autorun.env ]; then
+	echo "Error: autorun scripts must be run from within an initramfs VM"
+	exit 1
+fi
+
+. /vm_autorun.env
+
+_vm_ar_dyn_debug_enable
+
+creds_path="/tmp/cifs_creds"
+[ -n "$CIFS_DOMAIN" ] && echo "domain=${CIFS_DOMAIN}" >> $creds_path
+[ -n "$CIFS_USER" ] && echo "username=${CIFS_USER}" >> $creds_path
+[ -n "$CIFS_PW" ] && echo "password=${CIFS_PW}" >> $creds_path
+mount_args="-ocredentials=${creds_path}"
+[ -n "$CIFS_MOUNT_OPTS" ] && mount_args="${mount_args},${CIFS_MOUNT_OPTS}"
+set -x
+
+export PATH="${CIFS_UTILS_SRC}:${SAMBA_SRC}/bin:${PATH}"
+
+SAMBA_CONFIGFILE="$(smbd -b | grep -Po 'CONFIGFILE: \K.*$')"
+SAMBA_CONFIGDIR="$(dirname ${SAMBA_CONFIGFILE})"
+mkdir -p ${SAMBA_CONFIGDIR}
+
+cat > ${SAMBA_CONFIGFILE} << EOF
+[global]
+	workgroup = ${CIFS_DOMAIN}
+	witness : client version = 2
+EOF
+
+modprobe cifs || _fatal "failed to load cifs"
+
+swnd &
+
+mkdir -p /mnt/cifs
+mount -t cifs //${CIFS_SERVER}/${CIFS_SHARE} /mnt/cifs \
+	"$mount_args" || _fatal
+cd /mnt/cifs || _fatal
+set +x
---
+++ cut/cifs_witness.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#
+# Copyright (C) SUSE LINUX GmbH 2019, all rights reserved.
+#
+# This library is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation; either version 2.1 of the License, or
+# (at your option) version 3.
+#
+# This library is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+# License for more details.
+
+RAPIDO_DIR="$(realpath -e ${0%/*})/.."
+. "${RAPIDO_DIR}/runtime.vars"
+
+_rt_require_dracut_args
+
+"$DRACUT" --install "tail ps rmdir resize dd vim grep find df \
+		   mount.cifs ip ping getfacl setfacl truncate du \
+		   which touch cut chmod true false unlink \
+		   getfattr setfattr chacl attr killall sync \
+		   dirname seq basename fstrim chattr lsattr stat \
+		   file ldd logger \
+		   ${SAMBA_SRC}/bin/smbd \
+		   ${CIFS_UTILS_SRC}/swnd " \
+	--include "$RAPIDO_DIR/autorun/00-rapido-init.sh" \
+		  "/lib/dracut/hooks/emergency/00-rapido-init.sh" \
+	--include "$RAPIDO_DIR/autorun/cifs_witness.sh" "/root/.profile" \
+	--include "$RAPIDO_DIR/rapido.conf" "/rapido.conf" \
+	--include "$RAPIDO_DIR/vm_autorun.env" "/vm_autorun.env" \
+	--modules "bash base systemd systemd-initrd systemd-journald dracut-systemd" \
+	--add-drivers "cifs zram ccm gcm ctr" \
+	$DRACUT_EXTRA_ARGS \
+	$DRACUT_OUT || _fail "dracut failed"
+
+# assign more memory
+_rt_xattr_vm_resources_set "$DRACUT_OUT" "2" "1024M"
