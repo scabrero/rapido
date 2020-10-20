c41ef673d94e2eb8bc53df0f8f8d491b2f1ec750
Samuel Cabrero <scabrero@suse.de>
2020-10-20 18:16:04 +0200

    Add cifs_witness
    Signed-off-by: Samuel Cabrero <scabrero@suse.de>

---
+++ autorun/cifs_witness.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+#
+# Copyright (C) SUSE LINUX GmbH 2020, all rights reserved.
+#
+# This library is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation; either version 2.1 of the License, or
+# (at your option) version 3.
+#
+# This library is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+# License for more details.
+
+_vm_ar_env_check || exit 1
+
+_vm_ar_dyn_debug_enable
+_vm_ar_hosts_create
+
+cat > /etc/request-key.conf << EOF
+create  dns_resolver *          *               /usr/sbin/cifs.upcall -E %k
+EOF
+
+creds_path="/tmp/cifs_creds"
+[ -n "$CIFS_DOMAIN" ] && echo "domain=${CIFS_DOMAIN}" >> $creds_path
+[ -n "$CIFS_USER" ] && echo "username=${CIFS_USER}" >> $creds_path
+[ -n "$CIFS_PW" ] && echo "password=${CIFS_PW}" >> $creds_path
+mount_args="-ocredentials=${creds_path},witness"
+[ -n "$CIFS_MOUNT_OPTS" ] && mount_args="${mount_args},${CIFS_MOUNT_OPTS}"
+set -x
+
+export PATH="${CIFS_UTILS_SRC}:${SAMBA_SRC}/bin:${SAMBA_SRC}/sbin:${PATH}"
+
+# Create samba config file, necessary for swnd
+SAMBA_CONFIGFILE="$(smbd -b | grep -Po 'CONFIGFILE: \K.*$')"
+SAMBA_CONFIGDIR="$(dirname ${SAMBA_CONFIGFILE})"
+
+mkdir -p ${SAMBA_CONFIGDIR}
+cat > ${SAMBA_CONFIGFILE} << EOF
+[global]
+	netbios name = $(cat /proc/sys/kernel/hostname)
+	workgroup = ${CIFS_DOMAIN}
+	witness : client version = 2
+EOF
+
+# Load cifs and enable debug
+modprobe cifs || _fatal "failed to load cifs"
+echo 'module cifs +p' > /sys/kernel/debug/dynamic_debug/control
+echo 'file fs/cifs/* +p' > /sys/kernel/debug/dynamic_debug/control
+echo 7 > /proc/fs/cifs/cifsFYI
+
+# Setup RAM disk
+modprobe zram num_devices="1" || _fatal "failed to load zram module"
+echo "1G" > /sys/block/zram0/disksize || _fatal "failed to set zram disksize"
+filesystem="btrfs"
+mkfs.${filesystem} /dev/zram0 || _fatal "mkfs failed"
+mkdir -p /mnt/zram
+mount -t $filesystem /dev/zram0 /mnt/zram || _fatal
+
+dd if=/dev/zero of=/mnt/zram/1G.bin bs=1M count=512
+
+# Start systemd-journald, swnd logs to system journal
+ps -eo args | grep -v grep | grep /usr/lib/systemd/systemd-journald \
+	|| /usr/lib/systemd/systemd-journald &
+
+# Start swnd, the witness service user-space daemon
+swnd &
+
+ping -c 5 ${CIFS_SERVER}
+
+mkdir -p /mnt/cifs
+mount -t cifs //${CIFS_SERVER}/${CIFS_SHARE} /mnt/cifs \
+	"$mount_args" || _fatal
+
+set +x
---
+++ cut/cifs_witness.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#
+# Copyright (C) SUSE LINUX GmbH 2020, all rights reserved.
+#
+# This library is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation; either version 2.1 of the License, or
+# (at your option) version 3.
+#
+# This library is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+# License for more details.
+
+RAPIDO_DIR="$(realpath -e ${0%/*})/.."
+. "${RAPIDO_DIR}/runtime.vars"
+
+_rt_require_dracut_args "$RAPIDO_DIR/autorun/cifs_witness.sh"
+_rt_require_conf_dir SAMBA_SRC
+_rt_require_conf_dir CIFS_UTILS_SRC
+
+"$DRACUT" --install "$DRACUT_RAPIDO_INSTALL \
+		tail ps rmdir resize dd vim grep find df \
+		mount.cifs ip ping getfacl setfacl truncate du \
+		which touch cut chmod true false unlink \
+		getfattr setfattr chacl attr killall sync \
+		dirname seq basename fstrim chattr lsattr stat \
+		file ldd mkfs.btrfs tc rsync \
+		request-key cifs.upcall key.dns_resolver \
+		/usr/lib/systemd/systemd-journald journalctl \
+		${SAMBA_SRC}/sbin/smbd \
+		${CIFS_UTILS_SRC}/swnd " \
+	$DRACUT_RAPIDO_INCLUDES \
+	--add-drivers "cifs lzo lzo-rle btrfs zram ccm gcm ctr" \
+	--modules "bash base systemd systemd-initrd dracut-systemd network" \
+	$DRACUT_EXTRA_ARGS \
+	$DRACUT_OUT || _fail "dracut failed"
+
+# assign more memory
+_rt_xattr_vm_resources_set "$DRACUT_OUT" "2" "4096M"
